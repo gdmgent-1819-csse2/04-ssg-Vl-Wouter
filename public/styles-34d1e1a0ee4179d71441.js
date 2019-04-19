7f0d2615731ed6fc1b4f1b0686824eb859a24613
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2019-04-19 14:26:52 +0200

    removal of public from master

--- public/styles-34d1e1a0ee4179d71441.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{154:function(n,o,w){},158:function(n,o,w){},347:function(n,o,w){}}]);
-//# sourceMappingURL=styles-34d1e1a0ee4179d71441.js.map
--- public/styles-d3ef3d7c2c0fe06083df.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{103:function(n,o,c){},143:function(n,o,c){},144:function(n,o,c){},145:function(n,o,c){}}]);
-//# sourceMappingURL=styles-d3ef3d7c2c0fe06083df.js.map
